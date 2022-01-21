f410f37d4301b7544bbde8b53d88c2c65794902a
AlejandroAsensio <alejandro.asensio.alvarez@gmail.com>
2022-01-21 13:26:32 +0100

    ejericios jquery

---
+++ jQuery/js/ej1.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+// 1
+    $("div.module").css("background","blue");
+// 2
+    $("ul#myList li:eq(2)").css("background","red");
+    $("ul#myList").find("li").eq(2).css("background","red");
+    $("ul#myList li:even").eq(1).css("background","red");
+// 3
+    $("label[for=q]").css("color","red");
+        
+// 4
+    console.log($("*:hidden").length);
+// 5
+    console.log($("img").attr("alt").length)
+// 6
+    $("table tr:odd").css("background","green");
+
+})
